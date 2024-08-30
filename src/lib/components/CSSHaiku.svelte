7a066bd977f35e7083a3ba15d8ee3f6411caebe1
vernonian <121730895+vernonian@users.noreply.github.com>
2024-08-29 19:24:30 -0700

    Got haiku component to take in props

---
+++ src/lib/components/CSSHaiku.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+
+  export let haikuId: string;
+  export let haiku: string;
+</script>
+
+<div id={haikuId} class="css-haiku-box">
+  <pre>
+    {haiku}
+  </pre>
+</div>
+
+<style>
+.css-haiku-box {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 2rem;
+  border: solid 1px black;
+  font-size: 1rem;
+  font-family: monospace;
+  max-width: 350px;
+  min-height: 140px;
+  overflow: hidden;
+}
+
+.css-haiku {
+  padding-left: var(--gap-m);
+}
+
+.css-haiku-box pre {
+  transition: all 0.2s;
+}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,75 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+import CSSHaiku from "$lib/components/CSSHaiku.svelte";
+
+type haikuItem = {
+  id: number,
+  haikuId: string,
+  haikuText: string;
+}
+const haikus: haikuItem[] = [
+  {
+    id: 0,
+    haikuId: 'clearsky',
+    haikuText: `
+#clearsky {
+  background: dodgerblue;
+  opacity: nought point eight;
+  color: floralwhite;
+}`
+  },
+  {
+    id: 1,
+    haikuId: 'spacy',
+    haikuText: `
+#spacy {
+  text-align: center;
+  word-spacing: twenty v h;
+  overflow-x: scroll; 
+}`
+  },
+  {
+    id: 2,
+    haikuId: 'whelming',
+    haikuText: `
+#over #whelming {
+  line-height: point four six;
+  text-shadow: two rem zero
+  red; transform: scale(nine)
+}`
+  },
+  {
+    id:3,
+    haikuId: 'invert',
+    haikuText:`
+#invert {
+  background: #one one one;
+  transform: scale(minus one, one);
+  color: #EEE;
+}`
+  }
+
+
+];
+</script>
+
+<main>
+  <section class="section-wrap">
+    <div class="f-col f-s-s">
+      <h1>Welcome to CSS Haiku</h1>
+      <div>
+        <p>CSS haikus</p>
+        <p>an experimental thing</p>
+        <p>to celebrate styles.</p>
+      </div>
+    </div>
+  </section>
+  <section class="section_wrap">
+
+    <div class="g-col-4">
+      {#each haikus as haiku}
+       <!-- <CSSHaiku haikuId="clearsky">&Tab;background: dodgerblue;<br>opacity: nought point eight;<br>&Tab;color: floralwhite;</CSSHaiku> -->
+        <CSSHaiku haikuId={haiku.haikuId} haiku={haiku.haikuText}></CSSHaiku>
+      {/each}
+    </div>
+  </section>
+</main>
